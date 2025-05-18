27f2252d78553bbed3edb6df684d0328f7a3c382
ArpanParajuli <arpanparajuli394@gmail.com>
2025-05-18 22:41:38 +0545

    Second commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const fs = require("fs");
+
+const path = require("path");
+
 require("dotenv").config();
 const PORT =  process.env.PORT || 3000;
 
@@ -9,11 +13,26 @@ app.use(express.json());
 app.use(express.urlencoded({extended :true}));
 app.set('view engine' , 'ejs');
 
+  app.set('trust proxy', true);
 
 
 
-app.get("/" , (req,res) => {
-  res.render("index");
+app.get("/" , async (req,res) => {
+   const userIp = req.ip;
+   const userpath  = path.join(__dirname , 'user/userip.txt');
+  //  console.log(userpath);
+
+
+ await fs.appendFile(userpath , `User IP : ${userIp}` , (err)=>{
+    if(err){
+      console.error('Error appending to file:', err);
+    return;
+    }
+
+    console.log("User Data Saved Succesfully!");
+
+   } )
+  res.render("index" , {userIp :userIp });
 });
 
 
